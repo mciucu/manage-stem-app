d0c525dab825c43f013d846c445f9e39f8d4825e
Bogdan Ciobanu <bciobanu@yandex.com>
2017-08-08 15:04:05 +0300

    * Using redis instead of the rollup log file

--- commands/build.py
+++ commands/build.py
@@ -3,12 +3,22 @@ import subprocess
 import os
 import time
 from commands.base import BaseStemAppCommand
+from redis import StrictRedis
+
+# TODO: Find this from the project's settings.py
+REDIS_CONNECTION = {
+    "host": "localhost",
+    "port": 6379,
+    "db": 0,
+    "password": None,
+}
 
 
 class BuildStemAppCommand(BaseStemAppCommand):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.with_watch = kwargs.get("watch", False)
+        self.redis_connection = StrictRedis(**REDIS_CONNECTION)
 
     def run(self):
         self.run_command(["npm", "install"])
@@ -17,19 +27,18 @@ class BuildStemAppCommand(BaseStemAppCommand):
             commands = ["rollup", "-c"]
             if self.with_watch:
                 commands.append("--watch")
-            #self.run_command(commands, path=rollup_path)
-            file_name = self.get_project_path("stem-rollup.log")
-            process = subprocess.Popen(commands, stderr=open(file_name, "w+"), cwd=rollup_path)
+            process = subprocess.Popen(commands, stderr=subprocess.PIPE, cwd=rollup_path)
             while True:
-                reader = subprocess.Popen(["tail", "-n", "1", file_name], stdout=subprocess.PIPE)
-                message = reader.communicate()[0].decode()
-                if not (message == "" or message.startswith("bundl")):
-                    process.kill()
-                    process = subprocess.Popen(commands, stderr=open(file_name, "w+"), cwd=rollup_path)
-                else:
-                    if not self.with_watch:
-                        break
-                    time.sleep(2)
+                message = process.stderr.readline().decode()
+                if message:
+                    print(message, end="")
+                    if "bundling" in message:
+                        self.redis_connection.setex("bundle-ready", 3 * 60, "N")
+                    else:
+                        self.redis_connection.setex("bundle-ready", 60, "Y")
+                        if not self.with_watch:
+                            break
+                time.sleep(2)
 
         except KeyboardInterrupt:
             sys.exit("\rStopped building")
